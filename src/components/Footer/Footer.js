49429785d6e41b117880205bf3e42db18771276f
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-19 19:35:58 +0530

    Footer Component Complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,32 @@
 import React from "react";
 import "./Footer.css";
+import { email, footer_languages } from "../../config/body.json";
 function Footer() {
-  return <div className="footer">Footer</div>;
+  const renderLanguages = () => {
+    return footer_languages.map((language) => {
+      const { id, icon_src, name, icon_width } = language;
+      return (
+        <span key={id} className="footer__language--highlight">
+          <img
+            className="footer__icons"
+            src={icon_src}
+            alt={name}
+            style={{ width: icon_width, height: "auto" }}
+          />
+          {name}
+        </span>
+      );
+    });
+  };
+
+  return (
+    <div className="footer">
+      <div className="footer__language">
+        Made with love using {renderLanguages()}
+      </div>
+      <span className="footer__email">Hire Me -&gt; {email}</span>
+    </div>
+  );
 }
 
 export default Footer;
