310e6e0d428bb86971050bb744aab617f1bf4823
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-20 18:41:29 +0530

    Individual Project Showcase

--- src/components/Main/Projects/Project.js
+++ src/components/Main/Projects/Project.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "./Project.css";
 
-function Project() {
-  return <div>Project - Individual</div>;
+function Project({
+  projectName,
+  projectLogo,
+  projectTech,
+  projectDesc,
+  projectView,
+}) {
+  console.log(projectView);
+  const renderTechnology = () => {
+    return projectTech.map((tech, index) => {
+      return (
+        <p className="project__technology" key={index}>
+          {tech}
+        </p>
+      );
+    });
+  };
+
+  return (
+    <div className="project__container">
+      <div className="project__header">
+        <img className="project__logo" src={projectLogo} alt={projectName} />
+        <h1 className="project__title">{projectName}</h1>
+      </div>
+      <div className="project__description">{projectDesc}</div>
+      <div className="project__technologies_container">
+        {renderTechnology()}
+      </div>
+      <div className="project__view">
+        <img
+          className="project__icon"
+          src={projectView.icon_src}
+          alt={projectView.name}
+        />
+        <a className="project__link" href={projectView.link}>
+          View on {projectView.name}
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
--- src/components/Main/Projects/Projects.js
+++ src/components/Main/Projects/Projects.js
@@ -1,13 +1,31 @@
 import React from "react";
 import Project from "./Project";
 import "./Projects.css";
+import { projects } from "../../../config/body.json";
 
 function Projects() {
   return (
-    <div>
-      Projects - CONTAINER
-      <Project />
-      <Project />
+    <div className="projects">
+      {projects.map((project) => {
+        const {
+          id,
+          name,
+          logo,
+          technology_stack,
+          description,
+          project_view,
+        } = project;
+        return (
+          <Project
+            key={id}
+            projectName={name}
+            projectLogo={logo}
+            projectTech={technology_stack}
+            projectDesc={description}
+            projectView={project_view}
+          />
+        );
+      })}
     </div>
   );
 }
