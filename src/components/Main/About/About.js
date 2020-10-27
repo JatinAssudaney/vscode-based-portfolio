cc4fe9ef9bfb3cbe48e6424c7771d341dc7ba7e6
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-28 03:39:56 +0530

    About UI Improvements

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./About.css";
-// import "./About.prod.css";
+// import "./About.css";
+import "./About.prod.css";
 import { about_me } from "../../../config/body.json";
 
 function About() {
